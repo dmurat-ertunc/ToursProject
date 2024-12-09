2ed7c379791f70d813ab8e38506a059257087e7f
Dursun Murat <murat.ertunc456@outlook.com>
2024-12-09 23:37:26 +0300

    new

---
+++ ToursProject/Models/ToursModel/ApiResponseSingle.cs
@@ -0,0 +1,9 @@
+﻿namespace ToursProject.Models.ToursModel
+{
+    public class ApiResponseSingle<T>
+    {
+        public string Message { get; set; }
+        public bool Status { get; set; }
+        public T Data { get; set; }
+    }
+}
