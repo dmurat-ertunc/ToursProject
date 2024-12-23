58d6a475cd6a95267e962e16fc40639702dd99f7
Dursun Murat <murat.ertunc456@outlook.com>
2024-12-23 17:51:56 +0300

    kayıt olma tamamı ile bitti login işlemi yapılcak

---
+++ ToursProject/Models/Session/SignUpViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ToursProject.Models.Session
+{
+    public class SignUpViewModel
+    {
+        public string name {  get; set; }
+        public string surName {  get; set; }
+        public string mail {  get; set; }
+        public string userName {  get; set; }
+        public string password {  get; set; }
+        public string nationality {  get; set; }
+        public string tcno {  get; set; }
+    }
+}
